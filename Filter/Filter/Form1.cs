cf157b27b6f6425c35279cec98cd50d5318f7876
Skifok <taswdt@gmail.com>
2017-06-16 16:24:16 +0300

    Release

---
+++ Filter/Filter/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using MathNet.Numerics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Filter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)//open
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+            string Path = openFileDialog1.FileName;
+
+           Bm = new Bitmap(Path);
+
+            output f = new output(Bm);
+            f.ShowDialog();
+
+
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)//show
+        {
+            output f = new output(Bm);
+            f.ShowDialog();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)// black white
+        {
+            Bitmap filter = new Bitmap(Bm);
+            for (int x = 0; x < filter.Width; x++)
+                for (int y = 0; y < filter.Height; y++)
+                {
+                    Color c = filter.GetPixel(x, y);
+                    byte bw = (byte)(0.3 * c.R + 0.59 * c.G + 0.11 * c.B);
+
+                    filter.SetPixel(x, y, Color.FromArgb(c.A, bw, bw, bw));
+                }
+            output up = new output(filter);
+            up.ShowDialog();
+            Bm = filter;
+        }
+
+        private void button5_Click(object sender, EventArgs e) //impuls
+        {
+            Bitmap filter = new Bitmap(Bm);
+            Random r = new Random();
+            for (int x = 0; x < filter.Width; x+=r.Next()%50+50)
+                for (int y = 0; y < filter.Height; y+=r.Next()%50+50)
+                {
+                    Color c = filter.GetPixel(x, y);
+                    
+
+                    filter.SetPixel(x, y, Color.FromArgb(c.A,  255, 255, 255));
+                }
+            output up = new output(filter);
+            up.ShowDialog();
+            Bm = filter;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)//gauss
+        {
+            Bitmap filter = new Bitmap(Bm);
+            Random r = new Random();
+           // double[,] m = new double[1000,1000];
+             for (int x = 0; x < filter.Width; x += r.Next() % 10+5)
+                 for (int y = 0; y < filter.Height; y += r.Next() % 10+5)
+                 {
+
+
+                    double coef = Math.Abs(SampleGaussian(r, 0, 2));
+                    try
+                    {
+                        Color c = filter.GetPixel(x, y);
+                        filter.SetPixel(x, y,
+                            Color.FromArgb(c.A, (int)(c.R*coef), (int)(c.G * coef), (int)(c.B * coef)));
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                           
+                 }
+
+
+    
+            output up = new output(filter);
+            up.ShowDialog();
+            Bm = filter;
+        }
+        public static double SampleGaussian(Random random, double mean, double stddev)
+        {
+            // The method requires sampling from a uniform random of (0,1]
+            // but Random.NextDouble() returns a sample of [0,1).
+            double x1 = 1 - random.NextDouble();
+            double x2 = 1 - random.NextDouble();
+
+            double y1 = Math.Sqrt(-2.0 * Math.Log(x1)) * Math.Cos(2.0 * Math.PI * x2);
+            return y1 * stddev + mean;
+        }
+
+        private void button6_Click(object sender, EventArgs e)// gauss filet
+        {
+            double[,] gauss = new double[3, 3];
+            double sum = 0;
+            for(int i=0;i<3;i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    double index = i * i + j * j; //((i-1) * (i-1) + (j-1) * (j-1)) * -1;
+                    gauss[i, j] = Math.Pow(Constants.E,
+                        (index) / 18.0) / Constants.Pi2 / 9.0*10/1.9;    //1.47;
+                    sum += gauss[i, j];
+                }
+            Bitmap filter = new Bitmap(Bm);
+            for (int x = 1; x < filter.Width-1; x++)
+                for (int y = 1; y < filter.Height-1; y++)
+                {
+                    double coef = 0;
+                    for (int i = -1; i < 2; i++)
+                        for (int j = -1; j < 2; j++)
+                        {
+                            Color c1 = filter.GetPixel(x+i, y+j);
+                            coef += gauss[i + 1, j + 1] * c1.R;
+
+                        }
+                   // coef = coef / 9;
+                    Color c = filter.GetPixel(x, y);
+                    filter.SetPixel(x, y,
+                            Color.FromArgb(c.A, (int)coef, (int)coef, (int)coef));
+                }
+            output up = new output(filter);
+            up.ShowDialog();
+            Bm = filter;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)//mediana
+        {
+            Bitmap filter = new Bitmap(Bm);
+            for (int x = 1; x < filter.Width - 1; x++)
+                for (int y = 1; y < filter.Height - 1; y++)
+                {
+                    int[] mediana = new int[9];
+
+                    for (int i = 0; i < 3; i++)
+                        for (int j = 0; j < 3; j++)
+                        {
+                            Color c1 = filter.GetPixel(x + i-1, y + j-1);
+                            mediana[i * 3 + j] = c1.R;
+                            
+                        }
+                    Array.Sort(mediana);
+                    Color c = filter.GetPixel(x, y);
+                    filter.SetPixel(x, y,
+                            Color.FromArgb(c.A, mediana[4], mediana[4], mediana[4]));
+                }
+            output up = new output(filter);
+            up.ShowDialog();
+            Bm = filter;
+        }
+
+        private void button7_Click(object sender, EventArgs e)//rectangl
+        {
+            double[,] gauss = new double[3, 3];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    gauss[i, j] = 1.0/ 9.0;
+                }
+            Bitmap filter = new Bitmap(Bm);
+            for (int x = 1; x < filter.Width - 1; x++)
+                for (int y = 1; y < filter.Height - 1; y++)
+                {
+                    double coef = 0;
+                    for (int i = -1; i < 2; i++)
+                        for (int j = -1; j < 2; j++)
+                        {
+                            Color c1 = filter.GetPixel(x + i, y + j);
+                            coef += gauss[i + 1, j + 1] * c1.R;
+
+                        }
+                    // coef = coef / 9;
+                    Color c = filter.GetPixel(x, y);
+                    filter.SetPixel(x, y,
+                            Color.FromArgb(c.A, (int)coef, (int)coef, (int)coef));
+                }
+            output up = new output(filter);
+            up.ShowDialog();
+            Bm = filter;
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Filter/Filter/output.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Filter
+{
+    public partial class output : Form
+    {
+        public output(Bitmap bm)
+        {
+            InitializeComponent();
+            Width = bm.Width;
+            Height = bm.Height+150;
+
+            pictureBox1.Image = bm;
+            pictureBox1.Width= bm.Width;
+            pictureBox1.Height = bm.Height;
+            label1.Location = new Point(label1.Location.X, bm.Height + 30);
+            label2.Location = new Point(label2.Location.X, bm.Height + 30);
+            label3.Location = new Point(label3.Location.X, bm.Height + 80);
+            label4.Location = new Point(label4.Location.X, bm.Height + 80);
+            label1.Text = "Контрастность = " + Contrast(bm);
+            label2.Text = "Ср. кв. контрастность = " + sqContrast(bm);
+            label3.Text = "Полнота яркости = " + Polnota(bm);
+            label4.Text = "Уровень адаптации = " + adapt(bm);
+
+        }
+        public static double Contrast(Bitmap _bm)
+        {
+            int max = -1;
+            int min = 255 * 3;
+            for (int x = 0; x < _bm.Width; x++)
+                for (int y = 0; y < _bm.Height; y++)
+                {
+                    Color c = _bm.GetPixel(x, y);
+ 
+                    int current = c.R + c.G + c.B;
+
+                    if (current > max)
+                        max = current;
+                    if (min > current)
+                        min = current;
+
+                }
+            double d1 = max - min;
+            double d2 = max + min;
+            return d1/d2;
+        }
+        public static double sqContrast(Bitmap _bm)
+        {
+            double ave = 0;
+            for (int x = 0; x < _bm.Width; x++)
+                for (int y = 0; y < _bm.Height; y++)
+                {
+                    Color c = _bm.GetPixel(x, y);
+
+                    ave+= c.R + c.G + c.B;
+                }
+            ave = ave / _bm.Width / _bm.Height;
+
+            double res = 0;
+            for (int x = 0; x < _bm.Width; x++)
+                for (int y = 0; y < _bm.Height; y++)
+                {
+                    Color c = _bm.GetPixel(x, y);
+
+                    res += (c.R + c.G + c.B- ave)*(c.R + c.G + c.B - ave);
+                }
+            res=res/ _bm.Width / _bm.Height;
+            return Math.Sqrt(res);
+
+        }
+        public static double Polnota(Bitmap _bm)
+        {
+
+            int max = -1;
+            int min = 255 * 3;
+            for (int x = 0; x < _bm.Width; x++)
+                for (int y = 0; y < _bm.Height; y++)
+                {
+                    Color c = _bm.GetPixel(x, y);
+
+                    int current = c.R + c.G + c.B;
+
+                    if (current > max)
+                        max = current;
+                    if (min > current)
+                        min = current;
+
+                }
+
+            return 256.0/ (max + min);
+        }
+         public static double adapt(Bitmap _bm )
+        {
+            int max = -1;
+            double ave = 0;
+            for (int x = 0; x < _bm.Width; x++)
+                for (int y = 0; y < _bm.Height; y++)
+                {
+                    Color c = _bm.GetPixel(x, y);
+
+                    int current = c.R + c.G + c.B;
+                    ave += c.R + c.G + c.B;
+            if (current > max)
+                        max = current;
+                }
+            double d1= max/2;
+            ave = ave / _bm.Width / _bm.Height;
+            return 1- (ave - d1) / d1;
+        }
+    }
+}
